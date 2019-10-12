b23530d4b5e161c5b14c6dcb1ae80718de38219d
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2019-10-12 19:32:12 +0545

    Sales Section completed

--- src/krsuppliers/models/Particular.java
+++ src/krsuppliers/models/Particular.java
@@ -14,4 +14,21 @@ public class Particular {
     public Particular(String particular){
         this.particular = particular;
     }
+
+    @Override
+    public String toString() {
+        return this.particular;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public String getParticular() {
+        return particular;
+    }
+
+    public int getBalance_qty() {
+        return balance_qty;
+    }
 }
--- src/krsuppliers/sales/SaleController.java
+++ src/krsuppliers/sales/SaleController.java
@@ -1,88 +1,275 @@
 package krsuppliers.sales;
 
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
-import javafx.util.Callback;
 import krsuppliers.db.Database;
+import krsuppliers.models.Particular;
 import krsuppliers.models.Sale;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class SaleController {
     @FXML
-    Button save, delete, filter;
+    Button save, cancel, filter;
     @FXML
     TextField rate, qty, discount;
     @FXML
+    DatePicker from, to, date;
+    @FXML
     Text total;
+    @FXML
+    ChoiceBox<Particular> particular;
     @FXML
     TableView<Sale>table;
     @FXML
-    TableColumn<Sale, Integer> _qty, _amount, _discount, _rate;
+    TableColumn<Sale, Integer> _qty, _amount, _discount, _rate,_particular_id;
     @FXML
     TableColumn<Sale, String> _particular;
     @FXML
     TableColumn<Sale, Date> _date;
-    @FXML
-    DatePicker from, to;
+
+    private int sales_id = 0;
+
+    enum actions{
+        SAVE,
+        UPDATE
+    }
+
 
     private ObservableList<Sale> sales = FXCollections.observableArrayList();
+    private List<Particular> particulars = new ArrayList<>();
+    private actions STATE = actions.SAVE;
 
     @FXML
     public void initialize(){
+        particulars.clear();
+        sales.clear();
+        particular.getItems().clear();
+        clear();
+
+//        particular.setOnAction((e)->{
+//            System.out.println(particular.getSelectionModel().getSelectedItem());
+//        });
+
         setTableViewBinding();
-        filter.setOnAction((e)-> filterRecords());
-        retrieve("SELECT * FROM sales ORDER BY _id DESC LIMIT 20");
+        setParticulars();
+        filter.setOnAction(e-> filterRecords());
+        save.setOnAction(e->saveSales());
+        cancel.setOnAction(e->clear());
+
+        from.setValue(LocalDate.now());
+        to.setValue(LocalDate.now());
+        getSales("SELECT * FROM sales WHERE cancel = 0 ORDER BY _id DESC LIMIT 20");
     }
 
-    private void setTableViewBinding(){
-        table.setItems(sales);                                                  /* Observable List */
-        _qty.setCellValueFactory(new PropertyValueFactory<>("qty"));            /* Property Values */
-        _amount.setCellValueFactory(new PropertyValueFactory<>("amount"));
-        _discount.setCellValueFactory(new PropertyValueFactory<>("discount"));
-        _particular.setCellValueFactory(new PropertyValueFactory<>("particular"));
-        _rate.setCellValueFactory(new PropertyValueFactory<>("rate"));
-        _date.setCellValueFactory(new PropertyValueFactory<>("date"));
-    }
 
     private void filterRecords(){
         LocalDate start = from.getValue();
         LocalDate end = to.getValue();
-        if(start!=null && end!=null && start.isBefore(end)){
-            retrieve("SELECT * FROM sales WHERE date >= '" + start + "' AND date <= '" + end + "'");
+        if(start!=null && end!=null && !start.isAfter(end)){
+            getSales("SELECT * FROM sales WHERE date >= '" + start + "' AND date <= '" + end + "' AND cancel = 0");
         }
     }
 
-    private void retrieve(String queryString){
+    private void setParticulars(){
+        try{
+            Statement query = Database.getConnection().createStatement();
+            ResultSet resultSet = query.executeQuery("SELECT * FROM particulars");
+
+            while (resultSet.next()){
+                particulars.add(new Particular(
+                        resultSet.getInt("_id"),
+                        resultSet.getString("particular"),
+                        resultSet.getInt("balance_qty"))
+                );
+            }
+            particular.getItems().addAll(particulars);
+        }catch (SQLException e){
+            showErrorDialog(e.getMessage());
+        }
+    }
+
+    private void getSales(String queryString){
         try {
             Statement query = Database.getConnection().createStatement();
-            ResultSet results = query.executeQuery(queryString);
+            ResultSet resultSet = query.executeQuery(queryString);
             sales.clear();
             int _total = 0;
-            while (results.next()){
-                sales.add(new Sale(results.getInt("_id"),
-                        results.getDate("date"),
-                        results.getInt("particular_id"),
-                        results.getString("particular"),
-                        results.getInt("qty"),
-                        results.getInt("rate"),
-                        results.getInt("discount"),
-                        results.getInt("amount")));
-                _total += results.getInt("amount");
+            while (resultSet.next()){
+                sales.add(new Sale(resultSet.getInt("_id"),
+                        resultSet.getDate("date"),
+                        resultSet.getInt("particular_id"),
+                        resultSet.getString("particular"),
+                        resultSet.getInt("qty"),
+                        resultSet.getInt("rate"),
+                        resultSet.getInt("discount"),
+                        resultSet.getInt("amount")));
+                _total += resultSet.getInt("amount");
             }
 
-            total.setText(String.valueOf("Rs " + _total));
+            total.setText(String.valueOf(_total));
         }catch (SQLException e){
-            System.out.println(e.getMessage());
+            showErrorDialog(e.getMessage());
         }
     }
+
+    private void deleteSales(Sale sale){
+        try {
+            Statement query = Database.getConnection().createStatement();
+            int num = query.executeUpdate("DELETE FROM sales WHERE _id = " + sale.get_id());
+            if(num == 1){
+                sales.remove(sale);
+                int _total = Integer.parseInt(total.getText());
+                _total -= sale.getAmount();
+                total.setText(String.valueOf(_total));
+            }else{
+                showErrorDialog("Can't delete the selected record!");
+            }
+        }catch (SQLException e){
+            showErrorDialog(e.getMessage());
+        }
+    }
+
+    private void cancelSales(Sale sale){
+        try {
+            Statement query = Database.getConnection().createStatement();
+            int num = query.executeUpdate("UPDATE sales SET cancel = 1 WHERE _id = " + sale.get_id());
+            if(num == 1){
+                sales.remove(sale);
+                int _total = Integer.parseInt(total.getText());
+                _total -= sale.getAmount();
+                total.setText(String.valueOf(_total));
+            }else{
+                showErrorDialog("Can't restore the selected record!");
+            }
+        }catch (SQLException e){
+            showErrorDialog(e.getMessage());
+        }
+    }
+
+    private void updateSales(Sale sale){
+        STATE = actions.UPDATE;
+        sales_id = sale.get_id();
+        date.setValue(sale.getDate().toLocalDate());
+        qty.setText(String.valueOf(sale.getQty()));
+        rate.setText(String.valueOf(sale.getRate()));
+        discount.setText(String.valueOf(sale.getDiscount()));
+        particulars.forEach(t->{
+            if (t.get_id() == sale.getParticular_id()) {
+                particular.setValue(t);
+            }
+        });
+    }
+
+    private void saveSales(){
+        LocalDate date_ = date.getValue();
+        int qty_ = Integer.parseInt(qty.getText());
+        int rate_ = Integer.parseInt(rate.getText());
+        int discount_ = Integer.parseInt(discount.getText());
+        Particular p = particular.getValue();
+        int amt = qty_ * rate_ - discount_;
+
+        try {
+            if (STATE == actions.SAVE) {
+                PreparedStatement query = Database.getConnection().prepareStatement("INSERT INTO sales (date, particular_id, particular, qty, rate, amount, discount) VALUES(?,?,?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+                query.setDate(1, java.sql.Date.valueOf(date_));
+                query.setInt(2, p.get_id());
+                query.setString(3, p.getParticular());
+                query.setInt(4, qty_);
+                query.setInt(5, rate_);
+                query.setInt(6, amt);
+                query.setInt(7, discount_);
+                query.execute();
+                ResultSet resultSet = query.getGeneratedKeys();
+                if(resultSet.next()) {
+                    getSales("SELECT * FROM sales WHERE cancel = 0 ORDER BY _id DESC LIMIT 20");
+                    clear();
+                }else{
+                    showErrorDialog("Can't insert the sales record!");
+                }
+            }else if(STATE == actions.UPDATE && sales_id !=0){
+                PreparedStatement query = Database.getConnection().prepareStatement("UPDATE sales SET date = ?, particular_id = ?, particular = ?, qty = ?, rate = ?, amount = ?, discount = ? WHERE _id = ?" );
+                query.setDate(1, java.sql.Date.valueOf(date_));
+                query.setInt(2, p.get_id());
+                query.setString(3, p.getParticular());
+                query.setInt(4, qty_);
+                query.setInt(5, rate_);
+                query.setInt(6, amt);
+                query.setInt(7, discount_);
+                query.setInt(8, sales_id);
+                int num = query.executeUpdate();
+                if(num == 1){
+                    getSales("SELECT * FROM sales WHERE cancel = 0 ORDER BY _id DESC LIMIT 20");
+                    clear();
+                    sales_id = 0;
+                }else{
+                    showErrorDialog("Can't update the selected record!");
+                }
+            }
+        }catch (SQLException e){
+            showErrorDialog(e.getMessage());
+        }
+    }
+
+
+    private void setTableViewBinding(){
+        table.setItems(sales);
+
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem delete = new MenuItem("Delete");
+        MenuItem cancel = new MenuItem("Cancel");
+        MenuItem update = new MenuItem("Update");
+
+        update.setOnAction(e->updateSales(table.getSelectionModel().getSelectedItem()));
+        delete.setOnAction(e->deleteSales(table.getSelectionModel().getSelectedItem()));
+        cancel.setOnAction(e->cancelSales(table.getSelectionModel().getSelectedItem()));
+
+        contextMenu.getItems().addAll(update);
+        contextMenu.getItems().addAll(delete);
+        contextMenu.getItems().addAll(cancel);
+
+
+        table.setRowFactory((e)-> {
+                TableRow<Sale> row = new TableRow<>();
+                row.emptyProperty().addListener((observable, wasEmpty, isEmpty)->{row.setContextMenu(contextMenu);});
+                return row;
+        });
+
+        _qty.setCellValueFactory(new PropertyValueFactory<>("qty"));
+        _amount.setCellValueFactory(new PropertyValueFactory<>("amount"));
+        _discount.setCellValueFactory(new PropertyValueFactory<>("discount"));
+        _particular.setCellValueFactory(new PropertyValueFactory<>("particular"));
+        _particular_id.setCellValueFactory(new PropertyValueFactory<>("particular_id"));
+        _rate.setCellValueFactory(new PropertyValueFactory<>("rate"));
+        _date.setCellValueFactory(new PropertyValueFactory<>("date"));
+    }
+
+    private void clear(){
+        particular.setValue(null);
+        qty.setText("0");
+        date.setValue(LocalDate.now());
+        rate.setText("0");
+        discount.setText("0");
+        STATE = actions.SAVE;
+    }
+
+    private void showErrorDialog(String error){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Oops! Something went wrong!");
+        alert.setHeaderText(error);
+        alert.showAndWait();
+    }
+
+
 }
