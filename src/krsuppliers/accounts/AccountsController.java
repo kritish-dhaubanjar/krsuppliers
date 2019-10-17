2c53bea7ebb9e0632b210823a834d667812862e7
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2019-10-17 13:42:25 +0545

    Account Threaded

--- src/krsuppliers/accounts/AccountsController.java
+++ src/krsuppliers/accounts/AccountsController.java
@@ -2,6 +2,8 @@ package krsuppliers.accounts;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXSpinner;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
@@ -14,22 +16,29 @@ import java.io.IOException;
 import java.sql.*;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
 public class AccountsController {
     @FXML
     TextField database, user, password, host, port, newDatabase;
     @FXML
-    JFXCheckBox confirm;
+    JFXCheckBox confirm, populate_confirm;
     @FXML
-    JFXButton save, create;
+    JFXButton save, create, populate;
     @FXML
     Text confirm_msg;
+    @FXML
+    JFXSpinner busy;
 
     private Map<String, String> config;
 
     @FXML
     public void initialize(){
         save.setOnAction(e->update());
+        populate.setOnAction(e->populate_db());
+
+        busy.setVisible(false);
+
         create.setDisable(true);
         create.setOnAction(e->create());
         confirm.setSelected(false);
@@ -39,6 +48,14 @@ public class AccountsController {
             else
                 create.setDisable(true);
         });
+
+        populate_confirm.setOnAction(e->{
+            if (populate_confirm.isSelected())
+                populate.setDisable(false);
+            else
+                populate.setDisable(true);
+        });
+
         try {
             this.config = Configuration.getConfiguration().getConfig();
             this.database.setText(config.get("database"));
@@ -70,25 +87,83 @@ public class AccountsController {
         }
     }
 
-    private void create(){
-        if(confirm.isSelected()) {
-            try {
-                Statement statement = Database.getConnection().createStatement();
-                int num = statement.executeUpdate("CREATE DATABASE " + newDatabase.getText().trim());
-                if(num == 1){
-                    statement.executeUpdate("USE " + newDatabase.getText().trim());
-                    statement.execute("CREATE TABLE particulars (_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, particular TEXT NOT NULL)");
-                    statement.execute("CREATE TABLE purchases(_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, date DATE NOT NULL, particular_id INTEGER NOT NULL, particular TEXT NOT NULL, qty INTEGER NOT NULL, rate FLOAT NOT NULL, selling_rate FLOAT NOT NULL, discount FLOAT NOT NULL, amount FLOAT NOT NULL, cancel TINYINT NOT NULL DEFAULT 0)");
-                    statement.execute("CREATE TABLE sales(_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, date DATE NOT NULL, particular_id INTEGER NOT NULL, particular TEXT NOT NULL, qty INTEGER NOT NULL, rate FLOAT NOT NULL, discount FLOAT NOT NULL, amount FLOAT NOT NULL, cancel TINYINT NOT NULL DEFAULT 0)");
-                    showAlertDialog("Database created successfully!");
-                }else{
-                    showErrorDialog("Can't create database!");
-                }
-            } catch (SQLException e) {
-                showErrorDialog(e.getMessage());
-            }
-        }else{
+    private void populate_db(){
+        if(populate_confirm.isSelected()){
 
+            Task<String> task = new Task<String>() {
+                @Override
+                protected String call() throws Exception {
+                    try {
+                        Statement statement = Database.getConnection().createStatement();
+                        statement.executeUpdate("USE " + database.getText().trim());
+                        statement.execute("CREATE TABLE particulars (_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, particular TEXT NOT NULL)");
+                        statement.execute("CREATE TABLE purchases(_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, date DATE NOT NULL, particular_id INTEGER NOT NULL, particular TEXT NOT NULL, qty INTEGER NOT NULL, rate FLOAT NOT NULL, selling_rate FLOAT NOT NULL, discount FLOAT NOT NULL, amount FLOAT NOT NULL, cancel TINYINT NOT NULL DEFAULT 0)");
+                        statement.execute("CREATE TABLE sales(_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, date DATE NOT NULL, particular_id INTEGER NOT NULL, particular TEXT NOT NULL, qty INTEGER NOT NULL, rate FLOAT NOT NULL, discount FLOAT NOT NULL, amount FLOAT NOT NULL, cancel TINYINT NOT NULL DEFAULT 0)");
+                        return "Tables created successfully!";
+                    } catch (SQLException e) {
+                        return e.getMessage();
+                    }
+                }
+            };
+
+            task.setOnSucceeded(e->{
+                try {
+                    String result = task.get();
+                    if(result.equals("Database created successfully!")) {
+                        showAlertDialog(result);
+                    }else {
+                        showErrorDialog(result);
+                    }
+                }catch (InterruptedException | ExecutionException err){
+                    showErrorDialog(err.getMessage());
+                }
+            });
+
+
+            busy.visibleProperty().bind(task.runningProperty());
+            new  Thread(task).start();
+        }
+    }
+
+
+    private void create(){
+
+        if(confirm.isSelected()) {
+            Task<String> task = new Task<String>() {
+                @Override
+                protected String call() throws Exception {
+                    try {
+                        Statement statement = Database.getConnection().createStatement();
+                        int num = statement.executeUpdate("CREATE DATABASE " + newDatabase.getText().trim());
+                        if(num == 1) {
+                            statement.executeUpdate("USE " + newDatabase.getText().trim());
+                            statement.execute("CREATE TABLE particulars (_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, particular TEXT NOT NULL)");
+                            statement.execute("CREATE TABLE purchases(_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, date DATE NOT NULL, particular_id INTEGER NOT NULL, particular TEXT NOT NULL, qty INTEGER NOT NULL, rate FLOAT NOT NULL, selling_rate FLOAT NOT NULL, discount FLOAT NOT NULL, amount FLOAT NOT NULL, cancel TINYINT NOT NULL DEFAULT 0)");
+                            statement.execute("CREATE TABLE sales(_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, date DATE NOT NULL, particular_id INTEGER NOT NULL, particular TEXT NOT NULL, qty INTEGER NOT NULL, rate FLOAT NOT NULL, discount FLOAT NOT NULL, amount FLOAT NOT NULL, cancel TINYINT NOT NULL DEFAULT 0)");
+                            return "Database created successfully!";
+                        }
+                    } catch (SQLException e) {
+                        return e.getMessage();
+                    }
+                    return "Can't create database!";
+                }
+            };
+
+            task.setOnSucceeded(e->{
+                try {
+                    String result = task.get();
+                    if(result.equals("Database created successfully!")) {
+                        showAlertDialog(result);
+                    }else {
+                        showErrorDialog(result);
+                    }
+                }catch (InterruptedException | ExecutionException err){
+                    showErrorDialog(err.getMessage());
+                }
+            });
+
+            busy.visibleProperty().bind(task.runningProperty());
+            new  Thread(task).start();
         }
     }
 
